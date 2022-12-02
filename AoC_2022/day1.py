9e85537fff196169c8bee8a663c46664b6c7fb26
Vishwa-Mehta <vishwamehta2002@gmail.com>
2022-12-02 13:14:46 +0530

    smol changes

--- AoC_2022/day1.py
+++ AoC_2022/day1.py
@@ -8,14 +8,16 @@ for _ in sys.stdin:
 lines = line.split(',,')
 elves = []
 max_calories = 0
-top_elves = []
+top_elves = [] #part 2
 for _ in lines:
     _ = _.strip(',')
     l = _.split(',')
     elf = [int(calories) for calories in l]
     if sum(elf) > max_calories:
-        max_calories = sum(elf)
-        top_elves.append(max_calories)
+        max_calories = sum(elf) # part 1
+        top_elves.append(max_calories) # part 2
 
-print(sum(top_elves[-3:]))
+#part 1
 print(max_calories)
+#part 2
+print(sum(top_elves[-3:]))
